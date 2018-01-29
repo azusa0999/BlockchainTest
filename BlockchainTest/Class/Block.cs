ec9a8ed4eb2cb04cea18bac5ea2f30095ad12404
AzUsA <33446494+azusa0999@users.noreply.github.com>
2018-01-29 23:10:00 +0900

    프로젝트 업로드
    프로젝트 업로드합니다.

---
+++ BlockchainTest/Class/Block.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace BlockchainTest.Class
+{
+    public class Block
+    {
+        public Block(BlockHeader blockHeader, Object[] transactions)
+        {
+            this.blockHeader = blockHeader;
+            this.transactions = transactions;
+        }
+
+        private int blockSize;
+        private BlockHeader blockHeader;
+        private int transactionCount;
+        private object[] transactions;
+
+
+        public string getBlockHash()
+        {
+            byte[] bytes = blockHeader.toByteArray();
+            SHA256Managed hashstring = new SHA256Managed();
+            byte[] blockHash = hashstring.ComputeHash(bytes);
+            blockHash = hashstring.ComputeHash(blockHash);
+
+            return string.Join(",", blockHash);
+        }
+    }
+
+    public class BlockHeader
+    {
+        public BlockHeader(byte[] previousBlockHash, object[] transactions)
+        {
+            this.previousBlockHash = previousBlockHash;
+            this.merkleRootHash = transactions.GetHashCode();
+        }
+        
+        private int version;
+        private byte[] previousBlockHash;
+        private int merkleRootHash;
+        private int timestamp;
+        private int difficultyTarget;
+        private int nonce;
+
+        public byte[] toByteArray()
+        {
+            string tmpStr = "";
+            if (previousBlockHash != null)
+            {
+                tmpStr += string.Join(",", previousBlockHash);
+            }
+            tmpStr += merkleRootHash;
+            return Encoding.UTF8.GetBytes(tmpStr);
+        }
+    }
+}
---
+++ BlockchainTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlockchainTest.Class;
+
+namespace BlockchainTest
+{
+    class Program
+    {
+        //JAVA 코드로 이해하는 블록체인(BLOCKCHAIN) 
+        //링크 : http://guruble.com/java-%EC%BD%94%EB%93%9C%EB%A1%9C-%EC%9D%B4%ED%95%B4%ED%95%98%EB%8A%94-%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8blockchain/#comment-214
+        List<Block> blockchain = new List<Block>();
+
+        static void Main(string[] args)
+        {
+            // Genesis block
+            string[] transactions = { "DSKIM Sent 1k Bitcoins to ACS." };
+            // 최초 노드 : genesisBlock
+            BlockHeader blockheader = new BlockHeader(null, transactions);
+            Block genesisBlock = new Block(blockheader, transactions);
+            Console.WriteLine("Block Hash : {0}", genesisBlock.getBlockHash());
+            
+            // Transaction Forgery
+            string[] secondTransactions = new string[]{ "DSKIM Sent 10k Bitcoins to ACS."};
+            BlockHeader secondBlockheader = new BlockHeader(Encoding.UTF8.GetBytes(genesisBlock.getBlockHash()), secondTransactions);
+            Block secondBlock = new Block(secondBlockheader, secondTransactions);
+            Console.WriteLine("Second Block Hash : {0}", secondBlock.getBlockHash());
+
+            // Third block
+            String[] thirdTransactions = { "DSKIM Sent 500k Bitcoins to ACS." };
+            BlockHeader thirdBlockheader = new BlockHeader(Encoding.UTF8.GetBytes(genesisBlock.getBlockHash()), thirdTransactions);
+            Block thirdBlock = new Block(thirdBlockheader, thirdTransactions);
+            Console.WriteLine("Third Block Hash : {0}", thirdBlock.getBlockHash());
+
+            Console.Write("Any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
